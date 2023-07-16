2a0262174e15a17ede98bb60f3fbb093a71c7f5b
bsmandota <112806584+bsmandota@users.noreply.github.com>
2023-07-16 16:15:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 2024,
   },
   plugins: [react()],
 })
